1424721bbfe5a8f767ad34b42a68911a8242efda
Lasse Foster <lasse.foster@web.de>
2022-02-21 15:41:54 +0100

    Restructured build pipeline for better overview

--- source/Slipper_Editor/Program.cs
+++ source/Slipper_Editor/Program.cs
@@ -1,18 +1,17 @@
 ﻿using System.Runtime.InteropServices;
 
 namespace SlipperEditor{
-    public class Program
+    public unsafe class Program
     {
-        public const string EngineDll = @"../../../../Slipper_Engine/build/libSlipper_Engine.so";
+        public const string EngineDll = @"../../../Engine/libSlipper_Engine";
         
-
         [DllImport(EngineDll, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void say_hello();
+        public static extern void say_hello(int* array);
 
         static void Main(string[] args)
         {
             Console.WriteLine(File.Exists(EngineDll));
-            say_hello();
+            say_hello(null);
         }
     }
 }
--- source/Slipper_Engine/src/common_defines.h
+++ source/Slipper_Engine/src/common_defines.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#ifdef __GNUC__
 #define SLIPPER_FUNCTION __attribute__((dllexport))
+#elif _MSC_VER
+#define SLIPPER_FUNCTION _declspec(dllexport)
+#endif
 
 #ifdef __cplusplus
 #define EXTERNC extern "C"
