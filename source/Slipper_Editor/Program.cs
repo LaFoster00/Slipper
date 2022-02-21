37f9617bd555389569f881b582db77818535fa16
Lasse Foster <lasse.foster@web.de>
2022-02-22 00:14:26 +0100

    Reworked some of the build files for more reliable operation

--- source/Slipper_Editor/Program.cs
+++ source/Slipper_Editor/Program.cs
@@ -3,8 +3,12 @@
 namespace SlipperEditor{
     public unsafe class Program
     {
-        public const string EngineDll = @"../../../Engine/libSlipper_Engine";
-        
+#if DEBUG
+        public const string EngineDll = @"../../../Engine/Debug/libSlipper_Engine";
+#else
+        public const string EngineDll = @"../../../Engine/Release/libSlipper_Engine";
+#endif
+
         [DllImport(EngineDll, CallingConvention = CallingConvention.Cdecl)]
         public static extern void say_hello(int* array);
 
